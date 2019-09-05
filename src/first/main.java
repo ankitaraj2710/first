1c6e9ee00526615a4f7e4ec92d69e047e16031ff
ankita <macstudent@10.10.151.242>
2019-09-05 11:20:41 -0400

    Intial

---
+++ src/first/main.java
@@ -0,0 +1,19 @@
+package first;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("my github example project");
+		//take input from keyboard
+		System.out.println("Whats your name");
+		Scanner Keyboard = new Scanner(System.in);
+		//create a variable to store user input
+		String personName = Keyboard.nextLine();
+		//2 output a welcome msg
+		System.out.println("Hello" + personName);
+
+	}
+
+}
